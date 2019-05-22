8ec178d8f9504a89e772074c4fa2936e915b53c9
Oleg Jukovec <oleg.jukovec@gmail.com>
2019-05-23 00:41:02 +0300

    Adds default git init.

---
+++ git/init.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git config --global user.email "o.jukovec@orioledb.com"
+git config --global user.name "Oleg Jukovec"
+git config --global core.editor "nano"
+git config --global push.default simple
