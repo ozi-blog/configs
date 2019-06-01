438ade862164365d150ae052398a22bd2b3a084a
Oleg Jukovec <oleg.jukovec@gmail.com>
2019-06-01 15:18:49 +0300

    Changes default tabs width on git pager to 4.

--- git/init.sh
+++ git/init.sh
@@ -4,3 +4,4 @@ git config --global user.email "o.jukovec@orioledb.com"
 git config --global user.name "Oleg Jukovec"
 git config --global core.editor "nano"
 git config --global push.default simple
+git config --global core.pager 'less -x1,5'
